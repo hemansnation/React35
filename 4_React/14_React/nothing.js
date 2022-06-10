461654fb9ef65b8140fce1890a2da63a9c995d1e
Himanshu <himanshuramchandani08@gmail.com>
2022-06-11 00:04:42 +0530

    day 14 create-react-app

---
+++ 4_React/14_React/nothing.js
@@ -0,0 +1,19 @@
+// node.js - installation
+
+// https://nodejs.org/en/download/
+
+
+// npm
+
+// npx
+
+// create-react-app
+
+// VS code extensions
+// prettier, backet colorizer
+
+// globally install create-react-app in your system
+// npm install -g create-react-app
+
+// create a react app
+// npx create-react-app reactapp
